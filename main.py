0ab73e4cd10532b10327843073f88b7767a70981
Pedro Busato Ader <Pedro@MacBook-Pro-de-jorge.local>
2023-06-16 10:36:16 -0300

    Con alejo mejoramos el codigo. Agregamos la restriccion de los tiempos.

--- main.py
+++ main.py
@@ -21,17 +21,23 @@ def optimizationOne():
     P.set_objective('min', ( costSq.T * x * 75 + costSq.T * c * 6 + sum(c)*24000 ))              
 
     #Constrains
-    #Demand constrains --> How many demands can each truck satisfy
+    #Demand constrains --> Which trucks from each center can satisfy the demand of each
     dailyDemands = np.array([9,4,1,5,3,3,8])
-    for i in range(len(dailyDemands)):
-        P.add_constraint(c[i] * 2 )
+
+    P.add_constraint((c[0] + c[1] + c[2] + c[3] + c[4] + c[6] + c[7] + c[8] + c[9] + c[10] + c[11] + c[13] + c[14]) * 2 >= dailyDemands[0])
+    P.add_constraint((c[0] + c[1] + c[2] + c[7] + c[8] + c[9] + c[10]) * 2 >= dailyDemands[1])
+    P.add_constraint((c[2] + c[7] + c[8] + c[9] + c[10] + c[11] + c[12]+ c[13]) * 2 >= dailyDemands[2])
+    P.add_constraint((c[1] + c[2] + c[3] + c[4] + c[5] + c[6] + c[7] + c[8] + c[9] + c[10] + c[11] + c[12] + c[13] + c[14]) * 2 >= dailyDemands[3])
+    P.add_constraint((c[1] + c[2] + c[3] + c[4] + c[5] + c[6] + c[7] + c[10] + c[13] + c[14]) * 2 >= dailyDemands[4])
+    P.add_constraint((c[1] + c[2] + c[8] + c[9] + c[10]) * 2 >= dailyDemands[5])
+    P.add_constraint((c[2] + c[4] + c[5] + c[6] + c[7] + c[10] + c[11] + c[12] + c[13] + c[14]) * 2 >= dailyDemands[6])
 
     #Land constrains --> Is the place suitable for building the centre?
     for i in range(15):
-        P.add_constraint(x[i]*75 + c[i]*6 <= landAv[i])
+        P.add_constraint(c[i]*6 <= landAv[i] - 75)
 
     #Trucks constrains
-    
+
 
     print(P)
     # P.solve()
